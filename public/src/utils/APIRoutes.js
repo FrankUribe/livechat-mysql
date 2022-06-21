66ac13cb336d2a0a58c73aaf9aafc6ce8f13cc15
Frank Uribe <92763917+FrankUribe@users.noreply.github.com>
2022-06-21 18:02:44 -0500

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,4 @@
-// export const host = "http://aportealaingcivil.com/livechatapi";
-export const host = "https://chatapi.aportealaingcivil.com";
+export const host = "http://localhost:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const newChatUserRoute = `${host}/api/auth/newchatuser`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
